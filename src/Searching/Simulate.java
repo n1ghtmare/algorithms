62ed70b34f12b244d9148c07b2bc0c97e701a045
Dimitar Dimitrov <dimitar@nightosx.local>
2013-08-18 23:26:38 +0400

    Added a binary search

---
+++ src/Searching/Simulate.java
@@ -0,0 +1,15 @@
+package Searching;
+
+/**
+ *
+ * @author dimitar
+ */
+public class Simulate {
+    public static void main(String[] args) {
+        Integer[] numbers = new Integer[] { 1, 3, 7, 12, 15, 35, 77, 101, 179, 351 };
+        BinarySearch binarySearch = new BinarySearch(numbers);
+        int i = binarySearch.getIndexOf(3);
+        
+        System.out.println(String.format("The index is: %s", i));
+    }
+}
--- src/Sorting/Quick.java
+++ src/Sorting/Quick.java
@@ -7,15 +7,15 @@ package Sorting;
 public class Quick implements ISortingAlgorithm {
     @Override
     public void sort(Comparable[] a) {
-        // shuffle the array ... guarantees performance
         sort(a, 0, a.length - 1);
     }
     
+    // Running time - Average: O(n log(n))
+    // Running time - Worst: O(n^2)
     private void sort(Comparable[] a, int min, int max) {
-        if(max > 1) {
+        if(max > 1){
             int i = min, j = max;
             int mid = min + (max - min) / 2;
-            
             Comparable pivot = a[mid];
             
             while(i <= j) {
@@ -34,39 +34,14 @@ public class Quick implements ISortingAlgorithm {
                 }
             }
             
-            if(max > i){
+            if(i < max) {
                 sort(a, i, max);
             }
             
-            if(min < j) {
+            if(j > min) {
                 sort(a, min, j);
             }
         }
-    }
-    
-    // TODO ...
-    private int partition(Comparable[] a, int min, int max) {
-        int i = min, j = max;
-        int mid = min + (max - min) / 2;
-        
-        Comparable pivot = a[mid];
-        
-        while(i <= j) { 
-            while(a[i].compareTo(pivot) < 0) {
-                i++;
-            }
-            
-            while(a[j].compareTo(pivot) > 0) {
-                j--;
-            }
-            
-            if(i <= j){
-                swap(a, i, j);
-                i++;
-                j--;
-            }
-        }
-        return j;
     }
     
     private void swap(Comparable[] a, int i, int j) {
